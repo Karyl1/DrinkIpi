bdd5d6dfaac638e0b9644bfb8697af92a75b85c0
Chocoplou <john.ladeveze@gmail.com>
2020-01-21 15:25:10 +0100

    update Boissons and main

--- src/main/java/com/demo/Principale.java
+++ src/main/java/com/demo/Principale.java
@@ -12,7 +12,7 @@ import java.util.*;
  */
 public class Principale {
     public static void main(String[] args){
-        Boisson b1=new Boisson(10.2);
+        /*Boisson b1=new Boisson(10.2);
         Boisson b2=new Boisson(10.58);
         Boisson b3=new Boisson(9.2);
         Service s1=new Service(50);
@@ -27,7 +27,35 @@ public class Principale {
             total+=val.getPrix();
         }
         System.out.println("total des lignes service compris :" + total);
-        System.out.println(b1.getClass().getSimpleName());
+        System.out.println(b1.getClass().getSimpleName());*/
+
+        int counter = 0;
+        System.out.println("Avez-vous de la monnaie ? [o/n]");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        if(str.equals("n") || str.equals("N")) {
+            System.out.println("Merci de votre visite. A bientôt !");
+            System.exit(1);
+        }
+//        }else if (str.equals("o") || str.equals("O")){
+            System.out.println("Veuillez insérer votre monnaie :\n" +
+                    "La machine n'accepte que les pièces de 2€ mais rend la monnaie");
+            String str2 = sc.nextLine();
+            if(str2.equals("2")){
+                System.out.println("Veuillez choisir votre boisson :\n");
+
+                for (Boissons mark : Boissons.values()){
+                    counter++;
+                    System.out.println(mark.getName() + "\t" + mark.getPrice() + "\t" + counter);
+                }
+            }
+        /*}else{
+            System.out.println("Je n'ai pas compris votre réponse. A bientôt !");
+            System.exit(1);
+        }*/
+
+
     }
     
 }
